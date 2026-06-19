bbbab6917ea162698445e8e25260bf6e5039ae91
wetravelagencyjp-netizen <wetravelagencyjp@gmail.com>
2026-06-19 15:46:33 -0500

    feat: conectar AlertaCaducidad al alertaSlot del modulo retail

--- pos-saas/src/modules/retail/index.ts
+++ pos-saas/src/modules/retail/index.ts
@@ -1,12 +1,13 @@
 import type { ModuloConfig } from '@/core/types/modulos.types'
 import BarcodeScanner from './BarcodeScanner'
 import CatalogoProductos from '@/components/pos/CatalogoProductos'
+import AlertaCaducidad from './AlertaCaducidad'
 
 export const RetailModule: ModuloConfig = {
   displayName: 'Retail / Minimarket',
   topBarSlot:       BarcodeScanner,
   catalogoSlot:      CatalogoProductos,
   carritoSlot:       null,
-  alertaSlot:        null,
+  alertaSlot:        AlertaCaducidad,
   clienteExtraSlot:  null,
 }
