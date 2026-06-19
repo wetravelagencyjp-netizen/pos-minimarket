88cb425cb8d74840f5ebcd3cf5cb7d6e75daf636
wetravelagencyjp-netizen <wetravelagencyjp@gmail.com>
2026-06-19 12:40:58 -0500

    chore: remove duplicate next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-  async redirects() {
-    return [{ source: '/', destination: '/pos', permanent: false }]
-  },
-}
-
-export default nextConfig
